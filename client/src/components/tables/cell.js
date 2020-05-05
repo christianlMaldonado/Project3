5794aa5ea44ece44e5111da2f438e27b12a334bf
Matthew Bayers <mbayers6370@users.noreply.github.com>
2020-05-04 20:09:29 -0700

    update to tables

---
+++ client/src/components/tables/cell.js
@@ -0,0 +1,6 @@
+import React from "react";
+import TableCell from "@material-ui/core/TableCell";
+
+export default function Body(props) {
+  return <TableCell {...props} />;
+}
---
+++ client/src/components/tables/header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import TableHead from "@material-ui/core/TableHead";
+
+export default function Header(props) {
+  return <TableHead {...props} />;
+}
