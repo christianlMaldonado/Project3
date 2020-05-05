04dd78f9a2c3e34dba3d32c3e7691702da47ba6e
Matthew Bayers <mbayers6370@users.noreply.github.com>
2020-05-04 19:08:16 -0700

    update to tables component

--- client/src/pages/grades/grades.js
+++ client/src/pages/grades/grades.js
@@ -3,14 +3,15 @@ import "./style.css";
 import Tables from "../../components/tables/tables";
 
 class Grades extends Component {
-  
   render() {
     return (
       <>
         <div className="title">Grades</div>
         <div className="container">
           <div className="grades">
-            <div className="table-container"></div>
+            <div className="table-container">
+              <Tables />
+            </div>
           </div>
         </div>
       </>
