43a49fa8c59c8439aa9dd9ca686a1accedd9116b
niles87 <nbing19@gmail.com>
2020-05-06 18:14:54 -0700

    added a way to add students to the database

--- client/src/pages/create/create.js
+++ client/src/pages/create/create.js
@@ -11,6 +11,10 @@ class Create extends Component {
       user: undefined,
       assignment: "",
       description: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
     };
   }
   componentDidMount() {
@@ -47,36 +51,102 @@ class Create extends Component {
       this.setState({ assignment: "", description: "" });
     });
   };
+
+  addStudent = (e) => {
+    e.preventDefault();
+    const student = {
+      name: this.state.firstName,
+      email: this.state.email,
+      username: `${this.state.firstName} ${this.state.lastName}`,
+      password: this.state.password,
+    };
+    API.addStudent(student).then((res) => {
+      console.log(res);
+      this.setState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      });
+    });
+  };
   render() {
-    return (
-      <>
-        <div className="title">Create an Assignment</div>
-        <div className="container">
-          <div className="create">
-            <Form>
-              <Input
-                value={this.state.assignment}
-                onChange={this.handleInputChange}
-                name="assignment"
-                placeholder="Assignment name"
-              />
-              <Text
-                value={this.state.description}
-                onChange={this.handleInputChange}
-                name="description"
-                placeholder="Description"
-              />
-              <Btn
-                disabled={!this.state.assignment || !this.state.description}
-                onClick={this.handleFormSubmit}
-              >
-                Create Assignment
-              </Btn>
-            </Form>
+    if (this.state.user !== undefined) {
+      return !this.state.user.isStudent ? (
+        <>
+          <div className="title">Create an Assignment</div>
+          <div className="container">
+            <div className="create">
+              <h1>Add Assignments</h1>
+              <Form>
+                <Input
+                  value={this.state.assignment}
+                  onChange={this.handleInputChange}
+                  name="assignment"
+                  placeholder="Assignment name"
+                />
+                <Text
+                  value={this.state.description}
+                  onChange={this.handleInputChange}
+                  name="description"
+                  placeholder="Description"
+                />
+                <Btn
+                  disabled={!this.state.assignment || !this.state.description}
+                  onClick={this.handleFormSubmit}
+                >
+                  Create Assignment
+                </Btn>
+              </Form>
+
+              <h1>Add Students</h1>
+
+              <Form>
+                <Input
+                  value={this.state.firstName}
+                  onChange={this.handleInputChange}
+                  name="firstName"
+                  placeholder="first name"
+                />
+                <Input
+                  value={this.state.lastName}
+                  onChange={this.handleInputChange}
+                  name="lastName"
+                  placeholder="last name"
+                />
+                <Input
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                  name="email"
+                  placeholder="email"
+                  type="email"
+                />
+                <Input
+                  value={this.state.password}
+                  onChange={this.handleInputChange}
+                  name="password"
+                  placeholder="password"
+                  type="password"
+                />
+                <Btn
+                  disabled={!this.state.firstName || !this.state.lastName || !this.state.password}
+                  onClick={this.addStudent}
+                >
+                  Add Student
+                </Btn>
+              </Form>
+            </div>
           </div>
-        </div>
-      </>
-    );
+        </>
+      ) : (
+        <>
+          <div className="title">Create an Assignment</div>
+          <h3>Sorry {this.state.user.name}, you don't have access to this page</h3>
+        </>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
   }
 }
 
