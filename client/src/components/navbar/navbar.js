59822696098c9c22eb6f7e2f20fb29976c7d4e07
Matthew Bayers <mbayers6370@users.noreply.github.com>
2020-05-07 19:04:48 -0700

    update to fonts and page layout

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -16,28 +16,28 @@ function Navbar() {
           <a href="/home">
             <li>
               <i className="fa fa-home" data-hover="Home">
-                {isShown && "  Home"}
+                {isShown && <span className="addedLink"> Home</span>}
               </i>
             </li>{" "}
           </a>
           <a href="/grades">
             <li>
               <i className="fa fa-check-square" data-hover="Grades">
-                {isShown && "  Grades"}
+                {isShown && <span className="addedLink"> Grades</span>}
               </i>
             </li>
           </a>
           <a href="/attendance">
             <li>
               <i className="fa fa-calendar" data-hover="Attendance">
-                {isShown && "  Attendance"}
+                {isShown && <span className="addedLink"> Attendance</span>}
               </i>
             </li>
           </a>
           <a href="/assignments">
             <li>
               <i className="fa fa-pencil" data-hover="Assignments">
-                {isShown && "  Assignments"}
+                {isShown && <span className="addedLink"> Assignments</span>}
               </i>
             </li>
           </a>
@@ -46,7 +46,7 @@ function Navbar() {
           <a href="/" onClick={logout}>
             <li>
               <i className="fa fa-sign-out" data-hover="Assignments">
-                {isShown && "  Sign Out"}
+                {isShown && <span className="addedLink"> Sign Out</span>}
               </i>
             </li>
           </a>
